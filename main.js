3a145f3922cabef5cfeac47a97dc1310059b4e08
abdulbasid69 <55429465+abdulbasid69@users.noreply.github.com>
2020-07-11 12:51:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,11 @@
    var data = JSON.parse(text);
    console.log(data);
    details(data.basics);
+   career(data.Careerobjective);
+   education(data.education);
+   skills(data.skills);
+   achievements(data.achievements);
+   console.log(data);
    
  });
 
@@ -39,4 +44,66 @@ function details(det){
   var address = document.createElement("h4");
   address.textContent = det.address;
   child.appendChild(address);
+}
+var child2 = document.querySelector(".childtwo");
+function career(careerinfo){
+  var address = document.createElement("h2");
+  address.textContent = "Career Objective";
+  child2.appendChild(address);
+  var address = document.createElement("hr");
+  child2.appendChild(address);
+  var add = document.createElement("p");
+  add.textContent = careerinfo.info;
+  child2.appendChild(add);
+}
+function education(edu){
+  var ed = document.createElement("h2");
+  ed.textContent = "Education Qualifications";
+  child2.appendChild(ed);
+  var hr = document.createElement("hr");
+  child2.appendChild(hr);
+  for(i=0;i<edu.length;i++){
+    var deg = document.createElement("h3");
+    deg.textContent = edu[i].degree;
+    child2.appendChild(deg);
+    var eduul = document.createElement("ul");
+    var eduli = document.createElement("li");
+    eduli.textContent = edu[i].institute;
+    eduul.appendChild(eduli);
+    child2.appendChild(eduul);
+    var eduul = document.createElement("ul");
+    var eduli2 = document.createElement("li");
+    eduli2.textContent = edu[i].data;
+    eduul.appendChild(eduli2);
+    child2.appendChild(eduul);
+  }
+}
+function skills(tech){
+  var tec = document.createElement("h2");
+  tec.textContent = "Technical Skills";
+  child2.appendChild(tec);
+  var hr = document.createElement("hr");
+  child2.appendChild(hr);
+  var skilldata = document.createElement("table");
+  skilldata.border = "1";
+  child2.appendChild(skilldata);
+  tabledata="";
+  for(i=0;i<tech.length;i++){
+    tabledata+="<tr><td>"+tech[i].title+"</td><td>"+tech[i].data+"</td></tr>";
+  }
+  skilldata.innerHTML = tabledata;
+  }
+function achievements(ac){
+  var ach = document.createElement("h2");
+  ach.textContent ="Achievements";
+  child2.appendChild(ach);
+  var hr = document.createElement("hr");
+  child2.appendChild(hr);
+  for(i=0;i<ac.length;i++){
+    var ache = document.createElement("ul");
+    var ache1 = document.createElement("li");
+    ache1.textContent = ac[i].data;
+    ache.appendChild(ache1);
+    child2.appendChild(ache);
+  }
 }
